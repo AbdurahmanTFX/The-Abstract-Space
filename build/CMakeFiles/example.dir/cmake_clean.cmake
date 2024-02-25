5d56a30d64dae77d815a572b85518a5e02294e1f
AbdurahmanTFX <abdurahmanilahunov@gmail.com>
2024-02-25 21:38:45 +0300

    Start

---
+++ build/CMakeFiles/example.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/example.dir/core_basic_window.cpp.obj"
+  "CMakeFiles/example.dir/core_basic_window.cpp.obj.d"
+  "example.exe"
+  "example.exe.manifest"
+  "example.pdb"
+  "libexample.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/_deps/raylib-build/raylib/CMakeFiles/raylib.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Abdurahman/Documents/RayLibProjects/CMake/build/_deps/raylib-src/src/raudio.c" "_deps/raylib-build/raylib/CMakeFiles/raylib.dir/raudio.c.obj" "gcc" "_deps/raylib-build/raylib/CMakeFiles/raylib.dir/raudio.c.obj.d"
+  "C:/Users/Abdurahman/Documents/RayLibProjects/CMake/build/_deps/raylib-src/src/rcore.c" "_deps/raylib-build/raylib/CMakeFiles/raylib.dir/rcore.c.obj" "gcc" "_deps/raylib-build/raylib/CMakeFiles/raylib.dir/rcore.c.obj.d"
+  "C:/Users/Abdurahman/Documents/RayLibProjects/CMake/build/_deps/raylib-src/src/rmodels.c" "_deps/raylib-build/raylib/CMakeFiles/raylib.dir/rmodels.c.obj" "gcc" "_deps/raylib-build/raylib/CMakeFiles/raylib.dir/rmodels.c.obj.d"
+  "C:/Users/Abdurahman/Documents/RayLibProjects/CMake/build/_deps/raylib-src/src/rshapes.c" "_deps/raylib-build/raylib/CMakeFiles/raylib.dir/rshapes.c.obj" "gcc" "_deps/raylib-build/raylib/CMakeFiles/raylib.dir/rshapes.c.obj.d"
+  "C:/Users/Abdurahman/Documents/RayLibProjects/CMake/build/_deps/raylib-src/src/rtext.c" "_deps/raylib-build/raylib/CMakeFiles/raylib.dir/rtext.c.obj" "gcc" "_deps/raylib-build/raylib/CMakeFiles/raylib.dir/rtext.c.obj.d"
+  "C:/Users/Abdurahman/Documents/RayLibProjects/CMake/build/_deps/raylib-src/src/rtextures.c" "_deps/raylib-build/raylib/CMakeFiles/raylib.dir/rtextures.c.obj" "gcc" "_deps/raylib-build/raylib/CMakeFiles/raylib.dir/rtextures.c.obj.d"
+  "C:/Users/Abdurahman/Documents/RayLibProjects/CMake/build/_deps/raylib-src/src/utils.c" "_deps/raylib-build/raylib/CMakeFiles/raylib.dir/utils.c.obj" "gcc" "_deps/raylib-build/raylib/CMakeFiles/raylib.dir/utils.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
